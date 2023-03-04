ce1565c324c35b50dee2523bb6bddde10ae6eaa4
ryutaro-shimota <ryutaro.2000.radiology@gmail.com>
2023-03-04 19:29:42 +0900

    correct input.html, InputController.java, output.html

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,12 +1,16 @@
 package com.techacademy;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
     @GetMapping("/input")
-    public String getInput() {
+    public String getInput(@RequestParam(name="previous", required = false) String previous, Model model) {
+        model.addAttribute("previous", previous);
+
         return "input";
     }
 }
